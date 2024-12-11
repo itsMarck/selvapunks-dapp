414f99df12076ae81c7d502ddf01ba9d6f45f9ad
itsMarck <marcelofox08@gmail.com>
2024-12-11 13:38:28 -0500

    se modifico el index de home

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -4,28 +4,26 @@ import {
   Heading,
   Text,
   Button,
-  Image,
-  Badge,
+  Box,
   useToast,
 } from "@chakra-ui/react";
-import { Link } from "react-router-dom"; 
+import { Link, useHistory } from "react-router-dom";
 import { useWeb3React } from "@web3-react/core";
 import { useCallback, useEffect, useState } from "react";
 import useSelvaPunks from "../../hooks/useSelvaPunks";
 
 const Home = () => {
   const [isMinting, setIsMinting] = useState(false);
-  const { active } = useWeb3React();
+  const { active, account } = useWeb3React();
   const [maxSupply, setMaxSupply] = useState();
   const selvaPunks = useSelvaPunks();
-  const { account } = useWeb3React();
   const toast = useToast();
- 
+  const history = useHistory();
 
   const getMaxSupply = useCallback(async () => {
-    if(selvaPunks){
+    if (selvaPunks) {
       const result = await selvaPunks.methods.totalSupply().call();
-      setMaxSupply(result);  
+      setMaxSupply(result);
     }
   }, [selvaPunks]);
 
@@ -33,13 +31,13 @@ const Home = () => {
     getMaxSupply();
   }, [getMaxSupply]);
 
-  if(!active) return "Conecta tu wallet";
+  if (!active) return "Conecta tu wallet";
 
   const mint = () => {
     setIsMinting(true);
 
-    selvaPunks.methods.
-      mint(1)
+    selvaPunks.methods
+      .mint(1)
       .send({
         from: account,
         value: 0.001e18,
@@ -56,7 +54,7 @@ const Home = () => {
         toast({
           title: "Transacción confirmada",
           description: "¡Felicidades! Ahora eres dueño de un nuevo SelvaPunk.",
-          status: "success", 
+          status: "success",
         });
       })
       .on("error", (error) => {
@@ -69,95 +67,159 @@ const Home = () => {
       });
   };
 
-  return (
-    <>
-      <p>Existen: {maxSupply} SelvaPunks </p>
-      <Stack
-    align={"center"}
-    spacing={{ base: 8, md: 10 }}
-    py={{ base: 20, md: 28 }}
-    direction={{ base: "column-reverse", md: "row" }}
-  >
-    <Stack flex={1} spacing={{ base: 5, md: 10 }}>
-        <Heading
-          lineHeight={1.1}
-          fontWeight={600}
-          fontSize={{ base: "3xl", sm: "4xl", lg: "6xl" }}
-        >
-        <Text
-            as={"span"}
-            position={"relative"}
-            _after={{
-              content: "''",
-              width: "full",
-              height: "30%",
-              position: "absolute",
-              bottom: 1,
-              left: 0,
-              bg: "green.400",
-              zIndex: -1,
-            }}
-          >
-            Un SelvaPunk
-          </Text>
-          <br />
-          <Text as={"span"} color={"green.400"}>
-            una forma de ayudar
-          </Text>
-          </Heading>
-          <Text color={"gray.500"}>
-          Selvapunks son NFTs que viven en la blockchain, y que a traves de su 
-          comercialización permiten obtener fondos para salvar animales en peligro de extinción de la región Ucayali.
-        </Text>
-        <Stack
-          spacing={{ base: 4, sm: 6 }}
-          direction={{ base: "column", sm: "row" }}
-        >  
-        
-    <Button
-            rounded={"full"}
-            size={"lg"}
-            fontWeight={"normal"}
-            px={6}
-            colorScheme={"green"}
-            bg={"green.400"}
-            _hover={{ bg: "green.500" }}
-            disabled={!selvaPunks}
-            onClick={mint}
-            isLoading={isMinting}
-          >
-            
-            Obtén tu SelvaPunk
-          </Button>
-            <Button rounded={"full"}  size={"lg"} fontWeight={"normal"} px={6}>
-              <a href="https://testnets.opensea.io/account">Galería</a>
-            </Button>
-          
-          <Link to="/MarketSelvaPunks">
-            <Button rounded={"full"} size={"lg"} colorScheme={"blue"}
-            bg={"blue.400"} fontWeight={"normal"} px={6}>
-              Marketplace
-            </Button>
-          </Link>
-          </Stack>
-          </Stack>
-          <Flex
-        flex={1}
-        direction="column"
-        justify={"center"}
-        align={"center"}
-        position={"relative"}
-        w={"full"}
-      >
-        <iframe width="400" height="400" frameBorder="0" position={"absolute"} src={"https://imgflip.com/embed/62rl7o"} />
-        <p font-size="20px"><b>¡Uno de estos SelvaPunks puede ser tuyo!</b></p>
-      </Flex>
-      
-  </Stack> 
-  
-    </>
+  const navigateToMyNFTs = () => {
+    history.push(`/mis_nfts?address=${account}`);
+  };
 
-    
+  return (
+    <Box maxW="1200px" mx="auto" px={4} py={6}>
+      <Stack
+        align="center"
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 12, md: 20 }}
+        direction={{ base: "column-reverse", md: "row" }}
+      >
+        <Stack flex={1} spacing={{ base: 5, md: 8 }}>
+          <Heading
+            lineHeight={1.1}
+            fontWeight={600}
+            fontSize={{ base: "3xl", sm: "4xl", lg: "5xl" }}
+            textAlign={{ base: "center", md: "left" }}
+          >
+            <Text
+              as="span"
+              position="relative"
+              _after={{
+                content: "''",
+                width: "full",
+                height: "30%",
+                position: "absolute",
+                bottom: 1,
+                left: 0,
+                bg: "green.400",
+                zIndex: -1,
+              }}
+            >
+              Un SelvaPunk
+            </Text>
+            <br />
+            <Text as="span" color="green.400">
+              una forma de ayudar
+            </Text>
+          </Heading>
+          <Text
+            color="gray.500"
+            fontSize={{ base: "sm", md: "md" }}
+            textAlign={{ base: "center", md: "left" }}
+          >
+            Selvapunks son NFTs que viven en la blockchain y que, a través de
+            su comercialización, permiten obtener fondos para salvar animales
+            en peligro de extinción de la región Ucayali.
+          </Text>
+          <Stack
+            spacing={4}
+            direction="column"
+            alignItems="center"
+            width="100%"
+            maxW="400px"
+            mx="auto"
+          >
+            {/* First Line */}
+            <Stack direction={{ base: "column", sm: "row" }} spacing={4} width="100%">
+              <Button
+                rounded="full"
+                size="lg"
+                fontWeight="normal"
+                px={6}
+                colorScheme="green"
+                bg="green.400"
+                _hover={{ bg: "green.500" }}
+                disabled={!selvaPunks}
+                onClick={mint}
+                isLoading={isMinting}
+                width="100%"
+              >
+                Obtén tu SelvaPunk
+              </Button>
+              <Link to="/MarketSelvaPunks">
+                <Button
+                  rounded="full"
+                  size="lg"
+                  colorScheme="blue"
+                  bg="blue.400"
+                  fontWeight="normal"
+                  px={6}
+                  width="100%"
+                >
+                  Marketplace
+                </Button>
+              </Link>
+            </Stack>
+
+            {/* Second Line */}
+            <Stack direction={{ base: "column", sm: "row" }} spacing={4} width="100%">
+              <Link to="/Selvapunks">
+                <Button
+                  rounded="full"
+                  size="lg"
+                  fontWeight="normal"
+                  px={6}
+                  bg="var(--chakra-colors-gray-200)"
+                  _hover={{ bg: "var(--chakra-colors-gray-300)" }}
+                  width="100%"
+                >
+                  Galería
+                </Button>
+              </Link>
+              <Button
+                rounded="full"
+                size="lg"
+                fontWeight="normal"
+                px={6}
+                bg="var(--chakra-colors-gray-200)"
+                _hover={{ bg: "var(--chakra-colors-gray-300)" }}
+                width="100%"
+                onClick={navigateToMyNFTs}
+              >
+                Mis NFTs
+              </Button>
+              <Link to="/Financiamiento">
+                <Button
+                  rounded="full"
+                  size="lg"
+                  fontWeight="normal"
+                  px={6}
+                  bg="var(--chakra-colors-gray-200)"
+                  _hover={{ bg: "var(--chakra-colors-gray-300)" }}
+                  width="100%"
+                >
+                  Financiamiento
+                </Button>
+              </Link>
+            </Stack>
+          </Stack>
+        </Stack>
+        <Flex
+          flex={1}
+          justify="center"
+          align="center"
+          w="full"
+          maxW="400px"
+          mx={{ base: "auto", md: "0" }}
+        >
+          <Box
+            as="iframe"
+            src="https://imgflip.com/embed/62rl7o"
+            width="100%"
+            height="100%"
+            maxW="400px"
+            borderRadius="md"
+            allowFullScreen
+            style={{ aspectRatio: "1/1" }}
+          />
+        </Flex>
+      </Stack>
+    </Box>
   );
 };
 
